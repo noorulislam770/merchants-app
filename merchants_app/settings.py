57fd409db81cbd84b67c36e0071f921d53e1230b
Noor Ul Islam <noorulislam770@gmail.com>
2024-11-20 01:35:17 +0500

    added the render domain to allowed hosts

--- merchants_app/settings.py
+++ merchants_app/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-=&04+sm2nfdlm3r%4!+via^9vkm=du21+l8msf3=bt5$$m!tz^
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["merchants-app-72jn.onrender.com", "127.0.0.1"]
 
 
 # Application definition
